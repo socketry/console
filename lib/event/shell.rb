56790425e61bcfc1e927f51cafb7ca94e8cecf40
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2019-03-22 00:06:12 +1300

    Break circular loading.

--- lib/event/shell.rb
+++ lib/event/shell.rb
@@ -18,9 +18,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-require_relative 'terminal'
-require_relative 'buffer'
-
 require_relative 'generic'
 
 module Event
