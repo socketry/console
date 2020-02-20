30e242d8ece1a730f53d6e460fa444d67e9b0529
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2020-02-20 13:07:08 +1300

    Don't expose console recipe.

--- bake/console.rb
+++ bake/console.rb
@@ -1,19 +1,15 @@
 # frozen_string_literal: true
 
-def console
-	require_relative '../lib/console'
-end
-
 # Increase the verbosity of the logger to info.
 def info
-	self.console
+	require_relative '../lib/console'
 	
 	Console.logger.info!
 end
 
 # Increase the verbosity of the logger to debug.
 def debug
-	self.console
+	require_relative '../lib/console'
 	
 	Console.logger.debug!
 end
