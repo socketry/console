06c9de4adde7c84bcc10ecadd728dcd09db16b25
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2020-10-27 12:45:39 +1300

    Fix `Console::Capture` and add specs.

--- spec/console/capture_spec.rb
+++ spec/console/capture_spec.rb
@@ -1,4 +1,4 @@
-# Copyright, 2017, by Samuel G. D. Williams. <http://www.codeotaku.com>
+# Copyright, 2019, by Samuel G. D. Williams. <http://www.codeotaku.com>
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -18,54 +18,18 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-require_relative 'filter'
+require 'console/capture'
 
-module Console
-	# A general sink which captures all events into a buffer.
-	class Capture
-		def initialize
-			@buffer = []
-		end
-		
-		attr :buffer
-		
-		def last
-			@buffer.last
-		end
-		
-		def clear
-			@buffer.clear
-		end
-		
-		def verbose!(value = true)
-		end
-		
-		def call(subject = nil, *arguments, severity: UNKNOWN, **options, &block)
-			message = {
-				time: ::Time.now.iso8601,
-				severity: severity,
-				**options,
-			}
+RSpec.describe Console::Capture do
+	subject {described_class.new}
+	let(:logger) {Console::Logger.new(subject)}
+	
+	describe '#buffer' do
+		it 'can access log buffer' do
+			logger.info("Hello World!")
 			
-			if subject
-				message[:subject] = subject
-			end
-			
-			if arguments.any?
-				message[:arguments] = arguments
-			end
-			
-			if block_given?
-				if block.arity.zero?
-					message[:message] = yield
-				else
-					buffer = StringIO.new
-					yield buffer
-					message[:message] = buffer.string
-				end
-			end
-			
-			@buffer << message
+			last = subject.last
+			expect(last).to include(severity: :info, subject: "Hello World!")
 		end
 	end
 end
