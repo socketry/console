2d8b045356cfb246b776509b1b808f9485bd953c
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2025-02-20 09:32:54 +1300

    Modernize code.

---
+++ examples/simple/log.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+# Released under the MIT License.
+# Copyright, 2025, by Samuel Williams.
+
+require "console"
+
+Console.info(self, "Hello, world!", key: "value")
