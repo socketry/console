ecf4ad6e4025b4536d87d17f11cc062c02bdad59
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2026-07-10 18:55:36 +1200

    Improve null output coverage

---
+++ test/console/output/null.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# Released under the MIT License.
+# Copyright, 2026, by Samuel Williams.
+
+require "console/output/null"
+
+describe Console::Output::Null do
+	let(:output) {subject.new}
+	
+	it "is its own last output" do
+		expect(output.last_output).to be == output
+	end
+	
+	it "ignores calls" do
+		expect(output.call("Hello World")).to be_nil
+	end
+end
