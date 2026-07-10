904657cae77b3bbda28c834a0fb013b5e4ea1f5a
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2026-07-10 18:41:21 +1200

    Scope duration remainder calculations

--- lib/console/clock.rb
+++ lib/console/clock.rb
@@ -16,28 +16,28 @@ module Console
 				return format("%.2fs", duration)
 			end
 			
-			seconds = duration % 60
-			duration /= 60.0
+			minutes = duration / 60.0
 			
-			if duration < 60.0
-				return format("%dm%02ds", duration, seconds)
+			if minutes < 60.0
+				seconds = duration % 60
+				return format("%dm%02ds", minutes, seconds)
 			end
 			
-			minutes = duration % 60
-			duration /= 60.0
+			hours = minutes / 60.0
 			
-			if duration < 24.0
-				return format("%dh%02dm", duration, minutes)
+			if hours < 24.0
+				minutes = minutes % 60
+				return format("%dh%02dm", hours, minutes)
 			end
 			
-			hours = duration % 24
-			duration /= 24.0
+			days = hours / 24.0
 			
-			if duration < 100.0
-				return format("%dd%02dh", duration, hours)
+			if days < 100.0
+				hours = hours % 24
+				return format("%dd%02dh", days, hours)
 			end
 			
-			return format("%dd", duration)
+			return format("%dd", days)
 		end
 		
 		# @returns [Time] The current monotonic time.
